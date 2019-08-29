ab7d42d4ca803ceb914d913270a1946eb8c7cd94
Kadoarc <j.bungard@gmail.com>
2019-08-29 16:11:09 +1200

    Sound Manager

--- ViruscidePrototype/SoundManager.h
+++ ViruscidePrototype/SoundManager.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <list>
+
 class SoundManager
 {
 public:
 	SoundManager();
-	~SoundManager();
+	
+	void loadFromFile(const std::string &name, const std::string& filename);
+	void play(const std::string &name);
+
+private:
+
 };
 
